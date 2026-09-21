dcd645f806b992a23ac319d772d7cba13fbb252c
Ibrahim-Cygnus <olados11@gmail.com>
2026-09-22 02:05:35 +0500

    removed prints. now instead of simply showing response body, the response body gets converted to a hash using hashlib.sha256

--- robot.py
+++ robot.py
@@ -1,19 +1,12 @@
 import urllib.request
-
+import hashlib
 url = "https://example.com/"
 
 try:
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print(f'type of body before decoding {type(body)}')
-        body = response.read().decode('utf-8')
-        print("Success!")
-        print(f'type of response is {type(response)}')
-        status_code = response.status
-        print(f'status code is {status_code}')
-        print(body)
-        print(f'the type of the body after decoding is {type(body)}')
-        headers_dict = response.info()
-        print(headers_dict)
+        hash_object = hashlib.sha256(body)
+        hex_object = hash_object.hexdigest()
+        print(hex_object)
 except Exception as e:
     print(f"An error occurred: {e}")
