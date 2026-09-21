379e6b6522a41eba8940d957d390a23d34a38a54
Ibrahim-Cygnus <olados11@gmail.com>
2026-09-21 20:52:32 +0500

    added some prints to analyze the response and see what information it has

--- robot.py
+++ robot.py
@@ -1,31 +1,19 @@
-# import requests
-
-# url = "https://example.com/"
-
-# # Send a GET request
-# response = requests.get(url)
-
-# # Check if the request was successful (Status Code 200)
-# if response.status_code == 200:
-#     print("Success!")
-#     # If the response is HTML/Text:
-#     print(response.text)
-#     # If the response is JSON data:
-#     # print(response.json())
-# else:
-#     print(f"Failed with status code: {response.status_code}")
 import urllib.request
-import json
 
-# Define the target URL
 url = "https://example.com/"
 
 try:
-    # Send the GET request using a context manager
     with urllib.request.urlopen(url) as response:
-        # Read and decode the response body
+        body = response.read()
+        print(f'type of body before decoding {type(body)}')
         body = response.read().decode('utf-8')
         print("Success!")
+        print(f'type of response is {type(response)}')
+        status_code = response.status
+        print(f'status code is {status_code}')
         print(body)
+        print(f'the type of the body after decoding is {type(body)}')
+        headers_dict = response.info()
+        print(headers_dict)
 except Exception as e:
     print(f"An error occurred: {e}")
