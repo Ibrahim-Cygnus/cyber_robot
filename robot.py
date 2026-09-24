73c1ad92354c80e635049f2dc23f113e50c41de0
Ibrahim-Cygnus <olados11@gmail.com>
2026-09-24 21:29:27 +0500

    Implemented logging and timestamping hashes for different URLs using dictionary

--- robot.py
+++ robot.py
@@ -1,12 +1,24 @@
 import urllib.request
 import hashlib
-url = "https://example.com/"
+import datetime
+urls = ["https://example.com/", "https://demoqa.com/","https://example.com/", "https://demoqa.com/"]
+record = {}
+for url in urls:
+    try:
+        with urllib.request.urlopen(url) as response1:
+            body1 = response1.read()
+            hash_object1 = hashlib.sha256(body1)
+            hex_object1 = hash_object1.hexdigest()
+            timestamp = datetime.datetime.now(datetime.timezone.utc).astimezone()
+            if url in record:
+                if record[url][0] == hex_object1:
+                    print("no change")
+                else:
+                    print("change detected")
+                record[url][-1][1].append(timestamp.strftime("%Y-%m-%d %H:%M:%S.%f %Z %z"))
+            else:
+                record[url] = [[[hex_object1],[timestamp.strftime("%Y-%m-%d %H:%M:%S.%f %Z %z")]]]
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        hash_object = hashlib.sha256(body)
-        hex_object = hash_object.hexdigest()
-        print(hex_object)
-except Exception as e:
-    print(f"An error occurred: {e}")
+print(record)
